5ac4b20430a1c1fd108313497819a9e1989be121
Dnyaneshwari <dnyaneshwariumap994@gmail.com>
2022-10-02 17:29:34 +0530

    Added: Random dice input

--- snakeandladder/SnakeAndLadder.java
+++ snakeandladder/SnakeAndLadder.java
@@ -1,12 +1,21 @@
 package com.snakeandladder;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
-    static int position=0; //UC1
+
     public static void main(String[] args) {
 
 
         System.out.println("Welcome to SnakeAnd Ladder");
 
+        Random random = new Random();
+
+
+        int player = 0;// uc1
+        System.out.println("Player position => "+player);
+
+        int dice = random.nextInt(6)+1;// uc2
 
    
     }
