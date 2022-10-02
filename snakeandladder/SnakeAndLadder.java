eb3531ce567a0448e8aae45935a130e758d3e1b3
Dnyaneshwari <dnyaneshwariumap994@gmail.com>
2022-10-02 17:21:20 +0530

    Added: single player starting position

--- snakeandladder/SnakeAndLadder.java
+++ snakeandladder/SnakeAndLadder.java
@@ -1,7 +1,13 @@
 package com.snakeandladder;
 
 public class SnakeAndLadder {
+    static int position=0; //UC1
     public static void main(String[] args) {
+
+
         System.out.println("Welcome to SnakeAnd Ladder");
+
+
+   
     }
 }
